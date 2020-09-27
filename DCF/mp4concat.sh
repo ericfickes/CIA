3bb027b7d5a93183be95e408dbbc8e66b037be8d
Eric Fickes <ericfickes@gmail.com>
2020-09-27 13:34:25 -0600

    notes
    notes and scripts

--- DCF/mp4concat.sh
+++ DCF/mp4concat.sh
@@ -12,3 +12,7 @@ find . -name \*.$1 -printf "file '$PWD/%p'\n" | sort
 
 
 # ffmpeg -f concat -i <(find $2 -name "*.$1" -printf "file '$PWD/%p'\n" | sort) -c copy $3
+
+
+# THIS WORKS, port script to using this
+# ffmpeg -f concat -safe 0 -i <(for f in ./*.mp4; do echo "file '$PWD/$f'"; done) -c copy output.mp4
